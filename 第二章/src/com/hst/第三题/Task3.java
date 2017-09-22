566f6dff64a7f5edc3e93d804f99cc245f8f9eb6
Pudding <hu1song2tao3@gmail.com>
2017-09-22 20:41:43 +0800

    2017.09.22
    完成第二章第二、三题

---
+++ 第二章/src/com/hst/第三题/Task3.java
@@ -0,0 +1,75 @@
+﻿package com.hst.第三题;
+
+import java.util.Scanner;
+
+public class Task3 {
+	/**
+	 * 编写一个程序，给定一个t值（可初始化定义），按下式计算y值并输出，要求分别写出if语句和switch语句
+	 * 		t^2-1		0<=t<1
+	 * 		t^3-2t-2	1<=t<3
+	 * y=	t^2-t*sint  3<=t<5
+	 *  	t+1			5<=t<7
+	 * 		t-1			其他
+	 * @param args
+	 */
+	
+	private static double enbodyByIf(int t){
+		double y;
+		if (t>=0 && t<1) {
+			y = t*t - 1;
+		}
+		else if (t>=1 && t<3) {
+			y = t*t*t - 2*t - 2;
+		}
+		else if(t>=3 && t<5){
+			y = t*t - t*Math.sin(t);
+		}
+		else if (t>=5 && t<7) {
+			y = t + 1;
+		}
+		else{
+			y = t-1;
+		}
+		return y;
+	}
+	
+	private static double enbodyBySwitch(int t){
+		double y;
+		switch (t) {
+		case 0:
+			y = t*t - 1;
+			break;
+		case 1:
+		case 2:
+			y = t*t*t - 2*t - 2;
+			break;
+		case 3:	
+		case 4:	
+			y = t*t - t*Math.sin(t);
+			break;
+		case 5:	
+		case 6:
+			y = t + 1;
+			break;
+		default:
+			y = t-1;
+			break;
+		}
+		return y;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("请输入t值");
+		try{
+			Scanner input = new Scanner(System.in);
+			int t = input.nextInt();
+			double y1 = enbodyByIf(t);
+			double y2 = enbodyBySwitch(t);
+			System.out.println("if:"+y1+"\nswitch:"+y2);
+		}catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		
+	}
+
+}
---
+++ 第二章/src/com/hst/第二题/Task2.java
@@ -0,0 +1,38 @@
+﻿package com.hst.第二题;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Task2 {
+	/**
+	 * 编写一个程序，打印100~200之间的素数，要求每行按10个数（数与数之间有一个空格间隔）的形式对其输出
+	 * @param args
+	 */
+	private static void primerNumberGenerate(List<Integer> a){
+		for (int i = 100; i < 200; i++) {	//寻数范围
+			int j = 2;
+			while(j<i){		
+				if (i%j == 0) {		//出现能整除i的数则证明i不是素数，while循环结束
+					break;
+				}
+				j++;
+			}
+			if (i==j) {		//i与j相等时，证明不存在大于1且小于i的数能整除i
+				a.add(i);
+			}
+		}
+	}
+	public static void main(String[] args) {
+		List<Integer> container = new ArrayList<>();
+		primerNumberGenerate(container);
+		Iterator<Integer> i = container.iterator();
+		for (int j = 0; i.hasNext(); j++) {
+			if (j%10 == 0 && j!=0) {
+				System.out.println("\n");
+			}
+			System.out.print(i.next()+" ");
+		}
+	}
+
+}
