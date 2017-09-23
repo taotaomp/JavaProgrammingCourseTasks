28deaf3011cc0e93174f6c70bfed3c0c5f527807
Pudding <hu1song2tao3@gmail.com>
2017-09-23 20:04:43 +0800

    2017.09.23
    完成第二章第四、五题

---
+++ 第二章/src/com/hst/第五题/Task5.java
@@ -0,0 +1,32 @@
+﻿package com.hst.第五题;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+public class Task5 {
+	/**
+	 *编写一个Java应用程序，先产生一个随机数n（要求0<n<10),然后随机生成n个0~100的随机数，输出这n个随机数的和。
+	 * @param args
+	 */
+	private static void randomGenerate(List<Integer> a){
+		Random r = new Random();
+		int count = r.nextInt(10);
+		for (int i = 0; i < count; i++) {
+			a.add(r.nextInt(100));
+		}
+	}
+	
+	public static void main(String[] args) {
+		List<Integer> container = new ArrayList<>();
+		randomGenerate(container);
+		Iterator<Integer> i = container.iterator();
+		int sum = 0;
+		while (i.hasNext()) {
+			sum += i.next();
+		}
+		System.out.println(sum);
+	}
+
+}
---
+++ 第二章/src/com/hst/第四题/Task4.java
@@ -0,0 +1,34 @@
+﻿package com.hst.第四题;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Task4 {
+	/**
+	 * 用for语句实现下面的程序：从10~100能被2整除但不能被3整除的整数，要求每行按10个数（数与数之间有一个空格间隔）的形式输出
+	 * @param args
+	 */
+	
+	private static void forDemo(List<Integer> a){
+		for (int i = 10; i < 100; i++) {
+			if (i %2 == 0 && i%3 != 0) {
+				a.add(i);
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		List<Integer> container = new ArrayList<>();
+		forDemo(container);
+		Iterator<Integer> i = container.iterator();
+		for (int j = 0; i.hasNext(); j++) {
+			if (j%10 == 0&&j!=0) {
+				System.out.println("");
+			}
+			System.out.print(i.next()+ " ");
+		}
+
+	}
+
+}
